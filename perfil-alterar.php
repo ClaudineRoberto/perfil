c839952cc4bb81df245d98077f3ca46b84738199
Claudine Roberto Nicolau Junior <claudineroberto@hotmail.com>
2023-02-13 22:13:51 -0300

    Aula 13
    Aula 13

---
+++ perfil-alterar.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <title>Login</title>
+</head>
+<body>
+    
+    <section class="fundo">
+        <div class="container">
+            <div class="row w-50 ms-auto me-auto vh-100">
+                <div class="col align-self-center border rounded-3 p-3 fundoBranco">
+                    <form action="" class="">
+                        <div class="row">
+                            <div class="col">
+
+                                <input type="text" class="form-control mb-3" placeholder="Nome">
+
+                                
+                                <input type="email" class="form-control mb-3" placeholder="Email">
+
+                                
+                                <input type="text" class="form-control mb-3" placeholder="Profissão">
+
+      
+                                <input type="text" class="form-control mb-3" placeholder="Descrição">
+
+                                
+                                <input type="text" class="form-control mb-3" placeholder="Instagram">
+
+                              
+                                <input type="text" class="form-control mb-3" placeholder="Twitter">
+
+                               
+                                <input type="text" class="form-control mb-3" placeholder="Facebook">
+
+        
+                                <input type="text" class="form-control mb-3" placeholder="Linkedin">
+
+                               
+                                <input type="text" class="form-control mb-3" placeholder="Youtube">
+
+                      
+                                <input type="password" class="form-control mb-3" placeholder="Senha">
+                                
+                              
+                                <input type="text" class="form-control mb-3" placeholder="Foto">
+                               
+                        
+                                <input type="text" class="form-control mb-3" placeholder="Fundo">
+
+                                <button class="btn btn-primary mt-3">Salvar</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+
+
+
+
+
+
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>    
+</body>
+</html>
